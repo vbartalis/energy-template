d3525fe863c4fdad334c73a6efe971a4edcf8585
Bartalis Vilmos <vbartalis@gmail.com>
2024-02-09 00:20:03 +0100

    move service section and its components to a new directory.

---
+++ src/components/sections/services/services.data.ts
@@ -0,0 +1,59 @@
+import img1 from "../../../assets/solar3.jpg";
+import img2 from "../../../assets/wind.jpg";
+// TODO
+import img3 from "../../../assets/solar.jpg";
+import img4 from "../../../assets/grid.jpg";
+// TODO
+import img5 from "../../../assets/solar.jpg";
+import img6 from "../../../assets/green.jpg";
+
+export const servicesSectionData = {
+  title: "Our Sustainable Energy Solutions",
+  subheading: {
+    text1:
+      "Experience a Revolution in Energy Solutions - Embrace Innovation, Embrace Sustainability.",
+    text2:
+      "We're here to Empower Your Life and Spaces with Clean, Renewable Energy, Paving the Way for a Brighter and Greener Tomorrow.",
+  },
+  services: [
+    {
+      title: "Solar Power Solutions",
+      briefDescription:
+        "Harness the power of the sun with our state-of-the-art solar panels. Generate clean energy for your home or business and reduce your carbon footprint.",
+      visual: img1,
+    },
+    {
+      title: "Wind Energy Harvesting",
+      briefDescription:
+        "Embrace the power of the wind. Our wind energy solutions provide a sustainable and efficient way to generate electricity, ensuring a greener future.",
+      visual: img2,
+    },
+    {
+      title: "Hydropower Systems",
+      briefDescription:
+        "Flow with nature. Our hydropower systems harness the energy of flowing water, delivering reliable and eco-friendly electricity to your doorstep.",
+      visual: img3,
+    },
+    {
+      title: "Energy Storage Solutions",
+      briefDescription:
+        "Store excess energy efficiently. Our cutting-edge energy storage solutions ensure uninterrupted power supply, even during cloudy days or low-wind periods.",
+      visual: img4,
+    },
+    {
+      title: "Smart Grid Technology",
+      briefDescription:
+        "Navigate the future with our smart grid technology. Optimize energy distribution, monitor consumption, and contribute to a more sustainable and efficient energy ecosystem.",
+      visual: img5,
+    },
+    {
+      title: "Energy-efficient Lighting",
+      briefDescription:
+        "Illuminate your space responsibly. Our energy-efficient lighting solutions provide bright, sustainable, and cost-effective illumination for homes and businesses.",
+      visual: img6,
+    },
+  ],
+  callToAction:
+    "Discover the Future of Green Energy. Choose Your Sustainable Solution Today!",
+  buttonText: "Explore More",
+};
