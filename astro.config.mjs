7f4c317474fcf5af49ce229354ad01cd0a32e40f
Bartalis Vilmos <vbartalis@gmail.com>
2024-07-26 18:35:38 +0200

    Refactor Astro configuration and fix site URL typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: "https://https://energy.template.vbartalis.dev",
+  site: "https://energy.template.vbartalis.dev",
   base: "",
 });
